e60548d4735fcb288c8fe0d14cd065350a3023a5
PARKSANGHYUN <103012828+parksanghyun3@users.noreply.github.com>
2023-04-02 14:48:56 +0900

    15장 router 진행중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios"
 
 function App() {
+  // setNumber는 함수
+  // useState를 사용하면 리액트는 렌더링 됌
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    axios.post("http://localhost:3001/posts", {
+      title,
+      body
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input
+            className="form-control" value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }} />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Body</label>
+          <textarea
+            className="form-control" value={body}
+            onChange={(event) => {
+              setBody(event.target.value);
+            }}
+            rows="20" />
+        </div>
+        <button className="btn btn-primary" onClick={onSubmit}>Post</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
